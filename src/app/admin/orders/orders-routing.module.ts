9e4a4890646e701955e6d224d3c554763aa0158d
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-07 12:58:31 +0530

    Routing in orders-module

--- src/app/admin/orders/orders-routing.module.ts
+++ src/app/admin/orders/orders-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PendingOrdersComponent } from './pending-orders/pending-orders.component';
+import { ReturnOrdersComponent } from './return-orders/return-orders.component';
+import { SuccessfulOrdersComponent } from './successful-orders/successful-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'successful-orders',
+    component:SuccessfulOrdersComponent
+  },
+  {
+    path:'pending-orders',
+    component:PendingOrdersComponent
+  },
+  {
+    path:'return-orders',
+    component:ReturnOrdersComponent
+  },
+  {
+    path:'**',
+    component:SuccessfulOrdersComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

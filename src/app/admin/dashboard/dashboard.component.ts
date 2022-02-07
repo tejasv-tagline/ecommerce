58330d30ce0d578e34ae545d91303bdfe9a49e0b
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-07 12:12:03 +0530

    Added data to dashboard for navigating to other pages

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  public adminOptions=[
+    {
+      title:'Add Product',
+      text:'Click to add new product for sale',
+      button:'Add',
+      routerlink:'add-product'
+    },
+    {
+      title:'View All Products',
+      text:'Click to see all products',
+      button:'View',
+      routerlink:'view-all-product'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {

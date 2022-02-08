1febbbc2703c17c2b83ba5d9124a97656d2e7630
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-08 10:00:18 +0530

    Created customer module and dashboard component inside it

---
+++ src/app/customer/customer.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomerRoutingModule } from './customer-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    CustomerRoutingModule
+  ]
+})
+export class CustomerModule { }

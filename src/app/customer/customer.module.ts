01f52690312f17911f89240fd9ad4e46e2b562a9
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-08 14:16:49 +0530

    Customer module's html,css,ts,specs files created with ng g c customer --module=customer

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -8,6 +8,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CoreModule } from '../core/core.module';
 import { ProfileComponent } from './profile/profile.component';
 import { OrdersComponent } from './orders/orders.component';
+import { CustomerComponent } from './customer.component';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { OrdersComponent } from './orders/orders.component';
     ProductDetailsComponent,
     ProfileComponent,
     OrdersComponent,
+    CustomerComponent,
   ],
   imports: [
     CommonModule,

9cbfe2df64cacb14728db942132af0c8eb5e7369
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-08 09:55:39 +0530

    Showed all products added by admin

--- src/app/admin/view-all-product/view-all-product.component.ts
+++ src/app/admin/view-all-product/view-all-product.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-view-all-product',
   templateUrl: './view-all-product.component.html',
-  styleUrls: ['./view-all-product.component.scss']
+  styleUrls: ['./view-all-product.component.scss'],
 })
 export class ViewAllProductComponent implements OnInit {
-
-  constructor() { }
+  public basePath: any;
+  public allProduts:any=[];
+  constructor(private db: AngularFireDatabase) {}
 
   ngOnInit(): void {
-  }
+    this.basePath = this.db.database.ref('/products');
+    // let allProducts=this.basePath.on('value', (data: any) => {
+    //   data.val();
+    // });
 
+      this.basePath.on('value', (data: any) => {
+      this.allProduts = Object.keys(data.val()).map((key) => {
+        return {
+          ...data.val()[key],
+          push_key: key,
+        };
+      });
+    console.log('allProducts :>> ',this.allProduts );
+    })}
 }

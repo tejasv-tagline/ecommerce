41a7fc2060b944625458a095d0c5097b5de20a78
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-08 16:07:02 +0530

    Customer dashboard product showed

--- src/app/customer/dashboard/dashboard.component.ts
+++ src/app/customer/dashboard/dashboard.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  public basePath: any;
+  public allProducts: any;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+
+    this.basePath = this.db.database.ref('/products');
+    this.basePath.on('value', (data: any) => {
+      this.allProducts = Object.keys(data.val()).map((key) => {
+        return {
+          ...data.val()[key],
+          push_key: key,
+        };
+      });
+      // this.allProducts=data.val();
+      console.log('this.allProducts :>> ', this.allProducts);
+    });
+  }
 
   ngOnInit(): void {
+
   }
 
 }

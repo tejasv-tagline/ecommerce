1a4ee6bfca7119be730e3e1e800b99d725579fd0
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-08 12:23:58 +0530

    Cart and product details component created for customer in customer module

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    CartComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,

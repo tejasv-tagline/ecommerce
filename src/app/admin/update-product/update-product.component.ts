613f9f36d7bed578863a084e35a31b9fee774ed7
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-08 11:52:17 +0530

    Update product component done with added interface

--- src/app/admin/update-product/update-product.component.ts
+++ src/app/admin/update-product/update-product.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { AllProductList } from '../Interfaces/admin-common';
+import { CrudService } from '../Services/crud.service';
 
 @Component({
   selector: 'app-update-product',
   templateUrl: './update-product.component.html',
-  styleUrls: ['./update-product.component.scss']
+  styleUrls: ['./update-product.component.scss'],
 })
 export class UpdateProductComponent implements OnInit {
+  public pushKey!: string;
+  public basePath: any;
+  public myForm!: FormGroup;
+  public formData!: AllProductList;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private db: AngularFireDatabase,
+    private fb: FormBuilder,
+    private crudService: CrudService
+  ) {
+    this.pushKey = this.activatedRoute.snapshot.params['push_key'];
+    this.basePath = this.db.database.ref('/products/' + this.pushKey);
+    this.basePath.on('value', (data: any) => {
+      this.formData = data.val();
+    });
   }
 
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      title: [this.formData.title],
+      description: [this.formData.description],
+      price: [this.formData.price],
+      returnPeriod: [this.formData.returnPeriod],
+    });
+  }
+
+  public updateProduct(): void {
+    this.basePath.update(this.myForm.value);
+    // this.crudService.updateDetails(this.myForm.value);
+  }
 }

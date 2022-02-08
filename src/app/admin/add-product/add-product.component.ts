b893e0ed224c1c28b03e7ae506ff7c9bcf4e218b
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-08 10:01:06 +0530

    Created form for adding new product from admin side

--- src/app/admin/add-product/add-product.component.ts
+++ src/app/admin/add-product/add-product.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.scss']
+  styleUrls: ['./add-product.component.scss'],
 })
 export class AddProductComponent implements OnInit {
+  public myForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private db: AngularFireDatabase,private toaster:ToastrService) {
+    this.myForm = this.fb.group({
+      title: [''],
+      description: [''],
+      price: [''],
+      returnPeriod: [''],
+    });
   }
 
+  ngOnInit(): void {}
+
+  public onSubmit(): void {
+    const basePath = this.db.database.ref('/products');
+    basePath.push(this.myForm.value);
+    this.toaster.success('Product added ..!');
+
+    // console.log(this.myForm.value);
+  }
 }

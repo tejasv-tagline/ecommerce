b6baa0e7e8c30f09d27f3da5dfff3137f59b1c8e
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-08 18:05:02 +0530

    Single product detail showed and add to cart from cart service method called

--- src/app/customer/product-details/product-details.component.ts
+++ src/app/customer/product-details/product-details.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../services/cart.service';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
+  public basePath: any;
+  public id!: string;
+  public productDetails: any;
+  public pushProductToCart: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private db: AngularFireDatabase,
+    private cartService: CartService
+  ) {
+    this.id = this.activatedRoute.snapshot.params['push_key'];
+    this.basePath = this.db.database.ref('/products/' + this.id);
+    this.getProductDetails();
   }
 
+  ngOnInit(): void {}
+
+  public getProductDetails(): void {
+    this.basePath.on('value', (data: any) => {
+      this.productDetails = data.val();
+    });
+  }
+
+  public addToCart(): void {
+    this.cartService.addToCart(this.productDetails);
+  }
 }

42ec644ce03fe87a7e1f0c6f2f077d25d3b43a29
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-08 14:15:41 +0530

    Created customer header and modified routing of customer module

--- src/app/customer/customer-routing.module.ts
+++ src/app/customer/customer-routing.module.ts
@@ -1,20 +1,52 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { CustomerComponent } from './customer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {
     path:'',
-    component:DashboardComponent
-  },
-  {
-    path:'dashboard',
-    component:DashboardComponent
+    component:CustomerComponent,
+    children:[
+      {
+        path:'',
+        component:DashboardComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'cart',
+        component: CartComponent,
+      },
+      {
+        path: 'orders',
+        component: OrdersComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
+      {
+        path: 'product-details/:push_key',
+        component: ProductDetailsComponent,
+      },
+      {
+        path:'**',
+        redirectTo:'dashboard'
+      }
+    ]
   }
+  
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CustomerRoutingModule { }
+export class CustomerRoutingModule {}

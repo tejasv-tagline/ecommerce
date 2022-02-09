0fb3cc4fb814d70199161f8e02d3eec96cc0aeae
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-09 17:22:19 +0530

    Cart component done

--- src/app/customer/cart/cart.component.ts
+++ src/app/customer/cart/cart.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
+  public basePath: any;
+  public fullCartData: any;
+  public fullCartDataArray: any;
+  public ownCartData: any;
+  public changedCartData: any;
+  public baseCartPath: any;
+  public pushCartData: any;
+  public newBillCart: any;
+  public fullBillAmount!: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private db: AngularFireDatabase,
+    private cartService: CartService
+  ) {
+    this.basePath = this.db.database.ref('/cart');
+    this.basePath.on('value', (data: any) => {
+      this.fullCartData = data.val();
+      this.fullCartDataArray = Object.keys(data.val()).map((key) => {
+        return {
+          ...data.val()[key],
+          cartId: key,
+        };
+      });
+      this.ownCartData = this.fullCartDataArray.filter(
+        (cart: any) => cart.userid == localStorage.getItem('userid')
+      );
+      this.getOrderPrice();
+    });
   }
 
+  ngOnInit(): void {}
+
+  public changeQty(cartId: string, param: string): void {
+    this.cartService.changeQty(cartId, param);
+  }
+
+  public getOrderPrice(): void {
+    let orderPrice: any = [];
+    this.ownCartData.filter((e: any) => {
+      orderPrice.push(e.finalPrice);
+    });
+    this.fullBillAmount = eval(orderPrice.join('+'));
+  }
+
+  public removeProductFromCart(cartId: string) {
+    this.cartService.removeProductFromCart(cartId);
+  }
 }
--- src/app/customer/services/cart.service.ts
+++ src/app/customer/services/cart.service.ts
@@ -7,7 +7,17 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
 export class CartService {
   public userId: any;
   public basePath: any;
-
+  public fullCartData: any;
+  public fullCartDataArray: any;
+  public ownCartData: any;
+  public changedCartData: any;
+  public baseCartPath: any;
+  public pushCartData: any;
+  public newBillCart: any;
+  public fullBillAmount!: number;
+  // public qtyLessThen1:boolean=false;
+  public elseNewBillCart: any;
+  public isdisableDecrementButton: boolean = false;
   constructor(private db: AngularFireDatabase) {
     this.userId = localStorage.getItem('userid');
     this.basePath = this.db.database.ref('/cart');
@@ -16,8 +26,129 @@ export class CartService {
   public addToCart(productDetails: any): void {
     const cartData = {
       ...productDetails,
-      userid: this.userId
+      userid: this.userId,
+      finalPrice: productDetails.price * productDetails.qty,
     };
     this.basePath.push(cartData);
   }
+
+  public getOwnCart(): void {
+    // this.basePath = this.db.database.ref('/cart');
+    this.basePath.on('value', (data: any) => {
+      this.fullCartData = data.val();
+      this.fullCartDataArray = Object.keys(data.val()).map((key) => {
+        return {
+          ...data.val()[key],
+          cartId: key,
+        };
+      });
+      // console.log('this.fullCartDataArray :>> ', this.fullCartDataArray);
+      this.ownCartData = this.fullCartDataArray.filter(
+        (cart: any) => cart.userid == localStorage.getItem('userid')
+      );
+      // console.log('this.ownCartData :>> ', this.ownCartData);
+      this.getOrderPrice();
+    });
+    return this.ownCartData;
+  }
+
+  public changeQty(cartId: string, param: string) {
+    this.getOwnCart();
+    // console.log('this.ownCartData :>> ', this.ownCartData);
+    this.changedCartData = this.ownCartData.find(
+      (e: any) => e.cartId == cartId
+    );
+
+    this.baseCartPath = this.db.database.ref(
+      '/cart/' + this.changedCartData.cartId
+    );
+
+    this.pushCartData = {
+      cartId: this.changedCartData.cartId,
+      description: this.changedCartData.description,
+      finalPrice: this.changedCartData.finalPrice,
+      price: this.changedCartData.price,
+      qty: this.changedCartData.qty,
+      returnPeriod: this.changedCartData.returnPeriod,
+      title: this.changedCartData.title,
+      userid: this.changedCartData.userid,
+    };
+
+    if (param === 'plus') {
+      let dummyCartData = {
+        cartId: this.changedCartData.cartId,
+        description: this.changedCartData.description,
+        finalPrice: this.changedCartData.finalPrice,
+        price: this.changedCartData.price,
+        qty: this.changedCartData.qty + 1,
+        returnPeriod: this.changedCartData.returnPeriod,
+        title: this.changedCartData.title,
+        userid: this.changedCartData.userid,
+      };
+
+      this.baseCartPath.update(dummyCartData);
+      let passData = {
+        cartId: this.changedCartData.cartId,
+        description: this.changedCartData.description,
+        finalPrice: this.changedCartData.price * dummyCartData.qty,
+        price: this.changedCartData.price,
+        qty: dummyCartData.qty,
+        returnPeriod: this.changedCartData.returnPeriod,
+        title: this.changedCartData.title,
+        userid: this.changedCartData.userid,
+      };
+      this.baseCartPath.update(passData);
+      this.getOrderPrice();
+
+      // this.pushCartData.qty=this.pushCartData.qty+1;
+      // this.baseCartPath.update(this.pushCartData);
+      this.baseCartPath.on('value', (data: any) => {
+        this.newBillCart = data.val();
+      });
+    } else {
+      this.baseCartPath.on('value', (data: any) => {
+        this.elseNewBillCart = data.val();
+        // console.log('newBillCart :>> ', newBillCart);
+      });
+      
+        let dummyCartData = {
+          cartId: this.changedCartData.cartId,
+          description: this.changedCartData.description,
+          finalPrice: this.changedCartData.finalPrice,
+          price: this.changedCartData.price,
+          qty: this.changedCartData.qty - 1,
+          returnPeriod: this.changedCartData.returnPeriod,
+          title: this.changedCartData.title,
+          userid: this.changedCartData.userid,
+        };
+        this.baseCartPath.update(dummyCartData);
+
+        let passData = {
+          cartId: this.changedCartData.cartId,
+          description: this.changedCartData.description,
+          finalPrice: this.changedCartData.price * dummyCartData.qty,
+          price: this.changedCartData.price,
+          qty: dummyCartData.qty,
+          returnPeriod: this.changedCartData.returnPeriod,
+          title: this.changedCartData.title,
+          userid: this.changedCartData.userid,
+        };
+        // this.pushCartData.qty=this.pushCartData.qty - 1;
+        // this.baseCartPath.update(this.pushCartData);
+        this.baseCartPath.update(passData);
+        this.baseCartPath.on('value', (data: any) => {
+          this.newBillCart = data.val();
+        });
+        this.getOrderPrice();
+      
+    }
+  }
+
+  public removeProductFromCart(cartId:string){
+    const removeBasePath=this.db.database.ref('/cart/'+cartId);
+    removeBasePath.remove();
+  }
+
+
+  public getOrderPrice(): void {}
 }

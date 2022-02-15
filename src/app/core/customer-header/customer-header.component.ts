beeecf35455282b0a9512e6074a322cdec200f00
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-15 12:13:17 +0530

    Showed cart length on badge on customer header

--- src/app/core/customer-header/customer-header.component.ts
+++ src/app/core/customer-header/customer-header.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-customer-header',
   templateUrl: './customer-header.component.html',
-  styleUrls: ['./customer-header.component.scss']
+  styleUrls: ['./customer-header.component.scss'],
 })
 export class CustomerHeaderComponent implements OnInit {
+  public basePath: any;
+  public allCarts: any;
+  public cartLength:any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private db: AngularFireDatabase) {
+    this.basePath = this.db.database.ref('/cart');
+    this.basePath.on('value', (data: any) => {
+      this.allCarts = data.val();
+      // console.log('allCarts:>> ', this.allCarts);
+      this.allCarts = Object.keys(data.val()).map((key) => {
+        return {
+          ...data.val()[key],
+          productId: key,
+        };
+      });
+      this.cartLength = this.allCarts.filter(
+        (cart: any) => cart.userid == localStorage.getItem('userid')
+      );
+      console.log('this.cartLength :>> ', this.cartLength);
+    });
   }
 
+  ngOnInit(): void {}
 }

2dda915ac14432cdd272dfc4a37711be1bf22f84
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-15 10:23:26 +0530

    Modified routing and added common header,footer in common module's html file

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
+import { AdminComponent } from './admin.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -10,36 +11,39 @@ import { ViewAllProductComponent } from './view-all-product/view-all-product.com
 const routes: Routes = [
   {
     path:'',
-    component:DashboardComponent
+    component:AdminComponent,
+    children:[
+      {
+        path:'dashboard',
+        component:DashboardComponent
+      },
+      {
+        path:'profile',
+        component:ProfileComponent
+      },
+      {
+        path:'add-product',
+        component:AddProductComponent
+      },
+      {
+        path:'view-all-product',
+        component:ViewAllProductComponent
+      },
+      {
+        path:'update-product/:push_key',
+        component:UpdateProductComponent
+      },
+      {
+        path:'orders',
+        loadChildren:()=>import('../admin/orders/orders.module').then(m=>m.OrdersModule)
+      },
+      {
+        path:'**',
+        redirectTo:'dashboard'
+      }
+    ]
   },
-  {
-    path:'dashboard',
-    component:DashboardComponent
-  },
-  {
-    path:'profile',
-    component:ProfileComponent
-  },
-  {
-    path:'add-product',
-    component:AddProductComponent
-  },
-  {
-    path:'view-all-product',
-    component:ViewAllProductComponent
-  },
-  {
-    path:'update-product/:push_key',
-    component:UpdateProductComponent
-  },
-  {
-    path:'orders',
-    loadChildren:()=>import('../admin/orders/orders.module').then(m=>m.OrdersModule)
-  },
-  {
-    path:'**',
-    redirectTo:'dashboard'
-  }
+  
 ];
 
 @NgModule({

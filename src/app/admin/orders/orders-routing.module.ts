34e0b1705862eff95f7571a80a5a72cdb7507d8f
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-15 10:36:03 +0530

    Modified admin->orders module's routing and shifted common code from successfull-orders component to common component of admin->orders module

--- src/app/admin/orders/orders-routing.module.ts
+++ src/app/admin/orders/orders-routing.module.ts
@@ -1,27 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrdersComponent } from './orders.component';
 import { PendingOrdersComponent } from './pending-orders/pending-orders.component';
 import { ReturnOrdersComponent } from './return-orders/return-orders.component';
 import { SuccessfulOrdersComponent } from './successful-orders/successful-orders.component';
 
-const routes: Routes = [
-  {
-    path:'successful-orders',
-    component:SuccessfulOrdersComponent
-  },
-  {
-    path:'pending-orders',
-    component:PendingOrdersComponent
-  },
-  {
-    path:'return-orders',
-    component:ReturnOrdersComponent
-  },
-  {
-    path:'**',
-    component:SuccessfulOrdersComponent
-  },
-
+const routes: Routes = [{
+  path:'',
+  component:OrdersComponent,
+  children:[
+    {
+      path:'successful-orders',
+      component:SuccessfulOrdersComponent
+    },
+    {
+      path:'pending-orders',
+      component:PendingOrdersComponent
+    },
+    {
+      path:'return-orders',
+      component:ReturnOrdersComponent
+    },
+    {
+      path:'**',
+      component:SuccessfulOrdersComponent
+    },
+    
+  ]
+}
+ 
 ];
 
 @NgModule({

dbd1b3a32d9f2202c66d8721b061301f73b4412c
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-15 15:10:39 +0530

    Firebase deployement of project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD8dnW7tpA856OsUMZc2TZDbBygxlXR_RQ",
+    authDomain: "ecommerce-e6741.firebaseapp.com",
+    projectId: "ecommerce-e6741",
+    storageBucket: "ecommerce-e6741.appspot.com",
+    messagingSenderId: "500339100138",
+    appId: "1:500339100138:web:f972cd0fe86b180a604a93"
+  }
 };

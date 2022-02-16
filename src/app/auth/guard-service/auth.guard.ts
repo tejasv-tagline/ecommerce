66443fed5295c97fa85932e7ad526f511993010f
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-16 09:59:30 +0530

    Old auth service file deleted and new created due to name mismatch

--- src/app/auth/guard-service/auth.guard.ts
+++ src/app/auth/guard-service/auth.guard.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanDeactivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanDeactivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild, CanDeactivate<unknown>, CanLoad {
+  constructor(private authService:AuthService,private router:Router){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      if(!this.authService.getUserid()){
+        this.router.navigate(['login'])
+      }
     return true;
   }
   canActivateChild(

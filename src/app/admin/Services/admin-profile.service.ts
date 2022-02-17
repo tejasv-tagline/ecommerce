54c97ab947d684232c1b05a8669ee3fb5ab866a4
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-17 12:47:15 +0530

    Admin profile displayed on admin side

--- src/app/admin/Services/admin-profile.service.ts
+++ src/app/admin/Services/admin-profile.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminProfileService {
+  public basePath: any;
+  public allProfiles: any;
+  public userProfile: any;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+    this.basePath = this.db.database.ref('/users');
+  }
+
+  public getUserProfile(): void {
+    this.basePath.on('value', (data: any) => {
+      this.allProfiles = Object.keys(data.val()).map((key) => {
+        return {
+          ...data.val()[key],
+          userid: key,
+        };
+      });
+      this.userProfile = this.allProfiles.find(
+        (element: any) => element.userid == localStorage.getItem('userid')
+      );
+      return this.userProfile;
+    });
+  }
 }
--- src/app/admin/profile/profile.component.ts
+++ src/app/admin/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { ProfileService } from 'src/app/customer/services/profile.service';
+import { AdminProfileService } from '../services/admin-profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  public basePath:any;
+  public allUserProfile:any;
+  public userProfile:any;
 
-  constructor() { }
+  constructor(private db:AngularFireDatabase,private adminProfileService:AdminProfileService) {
+    this.basePath=this.db.database.ref('/users')
+    this.basePath.on('value',(data:any)=>{
+      this.allUserProfile = Object.keys(data.val()).map((key) => {
+        return {
+          ...data.val()[key],
+          userid: key,
+        };
+      });
+      this.userProfile=this.allUserProfile.find((element:any)=>
+      element.userid===localStorage.getItem('userid'))
+    })
+   }
 
   ngOnInit(): void {
+    this.adminProfileService.getUserProfile();
   }
+
 
 }

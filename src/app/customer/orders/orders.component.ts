b0ed525172cd947fcc9309c3af51fafae8377951
tejasv-tagline <tejasv.tagline@gmail.com>
2022-02-21 12:13:11 +0530

    working on return product

--- src/app/customer/orders/orders.component.ts
+++ src/app/customer/orders/orders.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { ToastrService } from 'ngx-toastr';
 import { OrderService } from '../services/order.service';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-orders',
@@ -11,8 +12,9 @@ import { OrderService } from '../services/order.service';
 export class OrdersComponent implements OnInit {
   public basePath:any;
   public fullCartValue:any;
+  public orderId: any;
 
-  constructor(public orderService:OrderService,private db:AngularFireDatabase,private toaster:ToastrService) { 
+  constructor(public orderService:OrderService,private db:AngularFireDatabase,private toaster:ToastrService,private productService:ProductService) { 
       this.orderService.getCutomerOwnOrders().then(()=>{
       this.toaster.success('All orders fetched successfully');
     });
@@ -27,6 +29,14 @@ export class OrdersComponent implements OnInit {
     this.basePath.on('value',(data:any)=>{
       this.fullCartValue=data.val().cartValue;
     })
+    console.log('this.fullCartValue :>> ', this.fullCartValue);
+    this.orderId=orderId;
+  }
+  
+  public returnProduct(productId:string):void{
+    console.log('ordeId :>> ', this.orderId);
+    
+    // this.productService.returnProduct(orderId,productId);
   }
 
 }
